4176e1ea735b5c7ef7284bef60ca51e2ffeed3bc
W1LLBEAN <apple@appledeiMac.local>
2016-03-26 13:36:37 +0800

    dwb1

---
+++ love/love/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  love
+//
+//  Created by apple on 16/3/26.
+//  Copyright © 2016年 bean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ love/love/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  love
+//
+//  Created by apple on 16/3/26.
+//  Copyright © 2016年 bean. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
